f55ef0725cd08ea8fc13082e767a9c5bdd28c7b7
yy3p9tw <yy3p9tw@gmail.com>
2025-07-14 11:45:03 +0800

    建立會員等入 尋找密碼 註冊會員

---
+++ 11401-BQUIZ02-main/front/login.php
@@ -0,0 +1,11 @@
+<fieldset>
+	<legend>會員登入</legend>
+	<form action="index.php?do=login" method="post">
+		帳號：<input type="text" name="acc" id="acc"><br>
+		密碼：<input type="password" name="pw" id="pw"><br>
+		<input type="button" value="登入">
+        <input type="reset" value="清除">
+        <a href="?do=forgot">忘記密碼</a>
+        <a href="?do=reg">尚未註冊</a>
+	</form>
+</fieldset>
---
+++ 11401-BQUIZ02-main/front/reg.php
@@ -0,0 +1,12 @@
+<fieldset>
+	<legend>會員註冊</legend>
+	<form>
+        <div class="ct" style="color:red">請設定您的帳號及密碼(最長12個字元)</div>
+	    Step1：登入帳號<input type="text" name="acc" id="acc"><br>
+	    Step2：登入密碼<input type="password" name="pw" id="pw"><br>
+        Step3：再次確認密碼<input type="password" name="pw2" id="pw2"><br>
+        Step4：信箱(忘記密碼時使用)<input type="text" name="email" id="email"><br>
+		<input type="button" value="註冊">
+        <input type="reset" value="清除">
+	</form>
+</fieldset>
