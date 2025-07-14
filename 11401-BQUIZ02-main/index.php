118fe6c6b241964c14b124c5933df139e548e356
yy3p9tw <yy3p9tw@gmail.com>
2025-07-14 11:25:26 +0800

    建立連結檔案

--- 11401-BQUIZ02-main/index.php
+++ 11401-BQUIZ02-main/index.php
@@ -39,6 +39,15 @@
                     	<a href="?do=login">會員登入</a>
                     </span>
                     <div class="">
+						<?php
+						$do=$_GET['do'] ?? 'main';
+						$file="./front/".$do.".php";
+						if(file_exists($file)){
+							include $file;
+						}else{
+							include "./front/main.php";
+						}
+						?>
                 	</div>
                 </div>
             </div>
